48713d1ff5be6c739c6a5fa954aa3c115936e420
Daniel Querol <dquerol@eventscase.com>
2021-11-26 22:35:44 +0100

    Testing service errors

--- source/internal/fetching/service_test.go
+++ source/internal/fetching/service_test.go
@@ -2,41 +2,59 @@ package fetching
 
 import (
 	"testing"
+	"fmt"
+
 	"github.com/stretchr/testify/assert"
 
 	"github.com/DanielQuerolBeltran/CliCryptoInfo/source/internal"
 	mockRepo "github.com/DanielQuerolBeltran/CliCryptoInfo/source/internal/storage/mocks"
 )
 
+const VALID_SYMBOL = "BTMT"
+const INVALID_SYMBOL = "ABCD"
 
-func TestGetCryptoById(t *testing.T) {
+func getCryptoMapFixtures() (map[string]cryptocli.Crypto) {
+	return  map[string]cryptocli.Crypto{
+		VALID_SYMBOL: cryptocli.Crypto{
+			Symbol: VALID_SYMBOL,
+			LastPrice: 1.01,
+			HighPrice: 1.001,
+			LowPrice:  1.12,
+			Volume:    0.1212,
+			LastId:    100,
+			FirstId:   101,
+		},
+	}
+} 
+
+
+func TestGetCryptoByIdShouldReturnACryptoEntity(t *testing.T) {
 	repo := new(mockRepo.MockRespository)
 	testService := NewService(repo)
 
-	symbol := "BTMT"
+	repo.On("GetAll").Return(getCryptoMapFixtures(), nil)
 
-	expectedCrypt := cryptocli.Crypto{
-		Symbol: symbol,
-		LastPrice: 1.01,
-		HighPrice: 1.001,
-		LowPrice:  1.12,
-		Volume:    0.1212,
-		LastId:    100,
-		FirstId:   101,
-	}
-
-	expectedCryptoMap := map[string]cryptocli.Crypto{
-		symbol: expectedCrypt,
-	}
-
-
-	repo.On("GetAll").Return(expectedCryptoMap, nil)
-
-	result, _ := testService.GetCryptoById(symbol)
+	result, _ := testService.GetCryptoById(VALID_SYMBOL)
 
 	repo.AssertExpectations(t)
 
-	assert.Equal(t, symbol, result.Symbol)
+	assert.Equal(t, VALID_SYMBOL, result.Symbol)
+}
+
+func TestGetCryptoByIdReturnAnError(t *testing.T) {
+	repo := new(mockRepo.MockRespository)
+	testService := NewService(repo)
+
+	expectedErrorMsg := fmt.Sprintf("Crypto %s not found", INVALID_SYMBOL)
+
+	repo.On("GetAll").Return(getCryptoMapFixtures(), nil)
+
+	result, resultErr := testService.GetCryptoById(INVALID_SYMBOL)
+
+	repo.AssertExpectations(t)
+
+	assert.Equal(t, cryptocli.Crypto{}, result)
+	assert.EqualErrorf(t, resultErr, expectedErrorMsg, "Error should be: %v, got: %v", expectedErrorMsg, resultErr)
 }
 
 
